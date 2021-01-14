e1dc8b8259ef079861fa93eb48cc38e78a349c8e
yokoyama takuji <gato.02010417@gmail.com>
2021-01-15 06:36:33 +0900

    環境設定

--- rojineko_view/nuxt.config.js
+++ rojineko_view/nuxt.config.js
@@ -56,7 +56,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -72,5 +72,11 @@ export default {
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    extend(config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.devtool = 'inline-cheap-module-source-map'
+      }
+    },
+  },
 }
